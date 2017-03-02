d35e27d50ff8a42bd8cf1fe5f4b640ac0738e11e
Szabó Péter <6030999+github@gmail.com>
2017-03-02 18:05:37 +0100

    Notice multiple troops arriving to the same destination

--- BombPlanTest.py
+++ BombPlanTest.py
@@ -56,5 +56,27 @@ class BombPlanTestCase(unittest.TestCase):
         self.assertIsNone(source)
         self.assertIsNone(target)
 
+    def testThePlan_bombTwoSmallTroopsTowardsNeutral(self):
+        plan = BombPlan(2)
+        uniprod = 0
+        ef1_garrison, ef2_garrison, = 0, 0
+        marching = self.calcBigArmyLimit(ef1_garrison, ef2_garrison) + 1
+        army1_size = marching // 2 - marching%2
+        army2_size = marching // 2 + marching%2
+        my_factory = Factory(0, US, 0, uniprod)
+        ef1 = Factory(1, THEM, ef1_garrison, uniprod)
+        ef2 = Factory(2, THEM, ef2_garrison, uniprod)
+        plan.registerSource(ef1, my_factory, 1, 99)
+        plan.registerSource(ef2, my_factory, 1, 99)
+        army1_to, army1_turns, army1_size = ef2.fid, 1, army1_size
+        army2_to, army2_turns, army2_size = ef2.fid, 1, army2_size
+        plan.registerTroop(army1_to, army1_turns, army1_size)
+        plan.registerTroop(army2_to, army2_turns, army2_size)
+        source, target = plan.thePlan(ef1_garrison + ef2_garrison + army1_size + army2_size)
+        self.assertIsNotNone(source)
+        self.assertIsNotNone(target)
+        self.assertEqual(source, my_factory)
+        self.assertEqual(target, ef2)
+
 if __name__ == '__main__':
     unittest.main()
